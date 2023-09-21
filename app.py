1b40500354bcd3fef6a27e3b0f5274c762b7f952
shubhamatkal <shubhamatkal333@gmail.com>
2023-09-21 14:26:00 +0530

    Include Header and Footer

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask , render_template
 import os
+from flask_bootstrap import Bootstrap5
 
 #initializing flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('FLASKKEY')
+Bootstrap5(app)
+
+#add a home page
+@app.route('/')
+def home_page():
+    return render_template('home.html')
 
 
 
